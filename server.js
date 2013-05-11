d46c0363ca73db30b0aa5d231d880afff1a0a16c
29decibel <mike.d.1984@gmail.com>
2013-05-10 19:46:21 -0700

    add simple express server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    app = express(),
+    RssGenerator = require("./lib/rss_generator").RssGenerator;
+
+app.get('/', function(req, res){
+
+  var rss = new RssGenerator();
+
+  rss.feeds(function(feeds){
+    res.charset = 'UTF-8';
+    res.header('Content-Type', 'text/xml');
+    res.send(feeds);
+  });
+
+});
+
+app.listen(3000);
